50f57ec5a7b6dbb1336a5febf06fb547205ccc27
SofianAcDev <sofian.achbabi@gmail.com>
2023-03-24 10:53:48 +0100

    CountDown init

---
+++ index.js
@@ -0,0 +1,31 @@
+let totalSeconds;
+let interval;
+
+function countDown() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const sec = seconds < 10 ? "0" + seconds : seconds;
+
+  countdownDisplay.innerHTML = `${minutes} : ${sec}`;
+
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.innerHTML = "Fin du compte à rebours";
+    clearInterval(interval);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (isNaN(choice.value)) {
+    alert("Entrez un nombre avant que je me fache");
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = "";
+    clearInterval(interval);
+    interval = setInterval(countDown, 100);
+  }
+});
